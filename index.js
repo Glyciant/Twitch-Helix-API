83e50f36cf4fcabde04d2b2d2d43866063d60944
Glyciant <glyciant@gmail.com>
2017-11-26 15:51:29 +0000

    V1.0.0 Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.clientID =  "";
+exports.authentication = require("./lib/authentication");
+exports.games = require("./lib/games");
+exports.streams = require("./lib/streams");
+exports.users = require("./lib/users");
+exports.videos = require("./lib/videos");
